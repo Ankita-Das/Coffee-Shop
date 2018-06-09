48e9c542523dd205ddbcc557bfae4a4c9a404aad
Ankita Das <ankidas44@gmail.com>
2018-06-09 11:08:37 +0530

    Create cappucino.php

---
+++ code/coffeetypes/cappucino.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Cappucino</title>
+</head>
+<body>
+<h1>CAPPUCINO</h1>
+<hr>
+
+<p>
+'Cappuccino' comes from Latin Caputium, later borrowed in German/Austrian and modified into 'kapuziner'. It is the diminutive form of cappuccio in Italian, meaning 'hood' or something that covers the head, thus 'cappuccino' reads 'small capuchin'. It is believed the capuchin friar, Marco d'Aviano, was the inspiration for this beverage
+</p>
+
+<img src="http://www.latteartguide.com/wp-content/uploads/2016/01/Cappuccino.jpg">
+<p>Rs. 150</p>
+
+<form action="cart.php" method="post">
+Name:<input type="text" name="itemname" value="Cappucino"><br>
+Quantity:<input type="number" name="quantity" min="0" value="0"><br>
+<input type="submit" value="Submit">
+<input type="reset">
+</form>
+
+
+</body>
+</head>
