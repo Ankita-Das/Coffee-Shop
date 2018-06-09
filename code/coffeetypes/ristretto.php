91a6ab7be4b4d214a32233ff8307b93cc7412a9e
Ankita Das <ankidas44@gmail.com>
2018-06-09 11:10:28 +0530

    Create ristretto.php

---
+++ code/coffeetypes/ristretto.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ristretto</title>
+</head>
+<body>
+<h1>RISTRETTO</h1>
+<hr>
+
+<p>
+Ristretto is traditionally a short shot of espresso coffee made with the normal amount of ground coffee but extracted with about half the amount of water in the same amount of time by using a finer grind. This produces a concentrated shot of coffee per volume. Just pulling a normal shot short will produce a weaker shot and is not a Ristretto as some believe.[1] The opposite of a ristretto is a lungo, which is typically double the shot volume. Ristretto means “limited” or “restricted” in Italian whereas lungo means “long.” The French equivalent of ristretto is café serré.
+</p>
+
+<img src="http://www.latteartguide.com/wp-content/uploads/2016/01/ristretto.jpg">
+<p>Rs. 100</p>
+
+<form action="cart.php" method="post">
+Name:<input type="text" name="itemname" value="Ristretto"><br>
+Quantity:<input type="number" name="quantity" min="0" value="0"><br>
+<input type="submit" value="Submit">
+<input type="reset">
+</form>
+
+
+</body>
+</head>
