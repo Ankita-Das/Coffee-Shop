43260adb9772525376e2bf342e0d9a25f6f2db66
Ankita Das <ankidas44@gmail.com>
2018-06-09 11:17:13 +0530

    Create exit.php

---
+++ code/exit.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Thanks for visiting!</title>
+</head>
+
+<body>
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "LIfeisyours!";
+$dbname = "cartDB";
+
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if($conn === false){
+	die("Connection failed:".mysqli_connect_error());
+}
+
+else{
+	//echo "Connection successful!";
+}
+
+
+$sql="DELETE FROM Mycart";
+//$sql = "SELECT * FROM Mycart";
+$result = $conn->query($sql);
+//echo "Number of rows: $result->num_rows<br>";
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name:";
+        echo $row["itemname"];   
+        echo "   Quantity:";
+	echo $row["quantity"];
+	echo"    Price:";
+	echo $row["price"];
+	echo"<br>";
+    }
+} else {
+    echo "Thank You for visiting.Do visit again!";
+}
+$conn->close();
+?>
+</body>
+</html>	
