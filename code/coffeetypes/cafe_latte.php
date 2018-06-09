a5856e6b198eb6a3c9b6dd5f4984be1ed2e1fd84
Ankita Das <ankidas44@gmail.com>
2018-06-09 11:11:16 +0530

    Create cafe_latte.php

---
+++ code/coffeetypes/cafe_latte.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Cafe Latte</title>
+</head>
+<body>
+<h1>CAFE LATTE</h1>
+<hr>
+
+<p>
+A latte is a coffee drink made with espresso and steamed milk.
+
+The term as used in English is a shortened form of the Italian caffè latte [kafˈfɛ ˈlatte], caffelatte [kaffeˈlatte] or caffellatte [kaffelˈlatte], which means "milk coffee". The word is also sometimes spelled latté or lattè in English with different kinds of accent marks, which can be a hyperforeignism or a deliberate attempt to indicate that the word is not pronounced according to the rules of English orthography.
+</p>
+
+<img src="http://www.latteartguide.com/wp-content/uploads/2016/01/cafe-latte.jpg">
+<p>Rs. 200</p>
+
+<form action="cart.php" method="post">
+Name:<input type="text" name="itemname" value="Cafe Latte"><br>
+Quantity:<input type="number" name="quantity" min="0" value="0"><br>
+<input type="submit" value="Submit">
+<input type="reset">
+</form>
+
+</body>
+</head>
