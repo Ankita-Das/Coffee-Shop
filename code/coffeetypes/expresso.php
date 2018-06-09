73b5509d67557c60b0e912ffacbd8f933060e57d
Ankita Das <ankidas44@gmail.com>
2018-06-09 11:09:42 +0530

    Create expresso.php

---
+++ code/coffeetypes/expresso.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Expresso</title>
+</head>
+<body>
+<h1>EXPRESSO</h1>
+<hr>
+
+<p>
+Espresso is coffee brewed by expressing or forcing out a small amount of nearly boiling water under pressure through finely ground coffee beans. Espresso is generally thicker than coffee brewed by other methods, has a higher concentration of suspended and dissolved solids, and has crema on top (a foam with a creamy consistency)
+</p>
+
+<img src="http://www.latteartguide.com/wp-content/uploads/2016/01/432856237_da68d53578_z.jpg">
+<p>Rs. 100</p>
+
+<form action="cart.php" method="post">
+Name:<input type="text" name="itemname" value="Expresso"><br>
+Quantity:<input type="number" name="quantity" min="0" value="0"><br>
+<input type="submit" value="Submit">
+<input type="reset">
+</form>
+
+
+</body>
+</head>
